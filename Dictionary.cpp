73a6a518ff33c8c25be986808902aa17610519e5
Eduard Iakovlev <yakovlevadn@mail.ru>
2023-09-27 15:47:18 +0700

    реализован поиск

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,9 +1,40 @@
 #include "Dictionary.h"
 
 // Возвращет новый узел с пустыми детьми
-Dictionary_liter* get_new_liter(void){
+Dictionary_liter* get_new_liter(void)
+{
+	struct Dictionary_liter* liter = new Dictionary_liter;
+	liter->end_word = false;
+	return nullptr;
+	for (int i = 0; i < size_alphabet; i++) {
+		liter->child[i] = nullptr;
+		return liter;
+	}
+}
 
-    struct Dictionarty_liter* liter = new Dictionarty_liter;
-    
-    return nullptr;
+// Вставляет ключ в дерево, если его нет, 
+void insert(Dictionary_liter* root, std::string key) {
+	Dictionary_liter* liter = root;
+
+	for (int i = 0; i < key.length(); i++) {
+		int index = key[i] - a;
+		if (!liter->child[index])
+			liter->child[index] = get_new_liter();
+
+		liter = liter->child[index];
+	}
+
+	liter->end_word = true;
+}
+
+// Возврашает true если ключ есть в дереве, иначе false
+bool search(Dictionary_liter* root, std::string key) {
+	struct Dictionary_liter* list = root;
+	for (int i = 0; i < key.length(); i++) {
+		int index = key[i] - a;
+		if (!list->child[index]) return false;
+		list = list->child[index];
+	}
+
+	return (list != nullptr && list->end_word);
 }
--- Dictionary.h
+++ Dictionary.h
@@ -2,6 +2,7 @@
 #include <string>
 
 const int size_alphabet{ 33 };
+const char a = 'а'; // русская а
 
 // структура словаря
 struct Dictionary_liter {
@@ -10,3 +11,5 @@ struct Dictionary_liter {
 };
 
 Dictionary_liter* get_new_liter(void);
+void insert(Dictionary_liter, std::string);
+bool search(Dictionary_liter*, std::string);
